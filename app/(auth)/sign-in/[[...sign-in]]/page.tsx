e752475647a1f5c4955c94019e3ff4dd5f43c879
Jayakrishnan M <jayakrishnan1012@gmail.com>
2024-09-09 23:01:12 +0530

    Refactor CSS classes in sign-in page component

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,4 +1,4 @@
-import { SignIn, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import { ClerkLoaded, ClerkLoading, SignIn } from "@clerk/nextjs";
 import { Loader2 } from "lucide-react";
 import Image from "next/image";
 
@@ -7,7 +7,7 @@ const SignInPage = () => {
     <div className="grid min-h-screen grid-cols-1 lg:grid-cols-2">
       <div className="h-full flex-col items-center justify-center px-4 lg:flex">
         <div className="space-y-4 pt-16 text-center">
-          <h1 className="text-3xl font-bold text-[#2E2A47]">Welcome back!</h1>
+          <h1 className="text-3xl font-bold text-white">Welcome back!</h1>
           <p className="text-base text-[#7E8CA0]">
             Log in or create account to get back to your dashboard.
           </p>
